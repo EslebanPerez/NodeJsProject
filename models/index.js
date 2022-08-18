3eb8f28d4ed0d5ab0d60d7f3e7c4b61a2e01d3c9
EslebanPerez <eslebanperez@gmail.com>
2022-08-18 02:55:05 -0500

    Agregando modelos para crear registros en la BD

---
+++ models/index.js
@@ -0,0 +1,7 @@
+const models = {
+    userModel : require('./nosql/user'),
+    tracksModel : require('./nosql/tracks'),
+    storageModel : require('./nosql/storage')
+}
+
+module.exports = models;
--- routes/tracks.js
+++ routes/tracks.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {getItems, getItem} = require('../controllers/tracks')
+const {getItems, getItem, createItem} = require('../controllers/tracks')
 
 //Aqui se creara la ruta de acceso http://localhost/tracks con los metodos GET, POST, DELETE, PUT
 
 router.get("/", getItems)
 
-router.get("/:id", getItem);
+router.post("/", createItem);
 
 module.exports = router;
