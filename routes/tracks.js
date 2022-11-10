2bd5565ab7c7dc9875e4d9a6e93e27315c012f69
EslebanPerez <eslebanperez@gmail.com>
2022-11-10 16:44:34 -0600

    Haciendo uso de matchedData para limpiar data del request y creando un manejador de errores

--- routes/tracks.js
+++ routes/tracks.js
@@ -7,6 +7,6 @@ const {validatorCreateItem} = require("../validators/tracks")
 
 router.get("/", getItems)
 
-router.post("/", validatorCreateItem, customHeader, createItem);
+router.post("/", validatorCreateItem, createItem);
 
 module.exports = router;
