a8881c62e243592f311315611333fb1a0f0307c1
EslebanPerez <eslebanperez@gmail.com>
2022-11-10 15:02:09 -0600

    Agregando validaciones en registro de tracks con ExpressValidator

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const {getItems, getItem, createItem} = require('../controllers/tracks')
-
+const {validatorCreateItem} = require("../validators/tracks")
 //Aqui se creara la ruta de acceso http://localhost/tracks con los metodos GET, POST, DELETE, PUT
 
 router.get("/", getItems)
 
-router.post("/", createItem);
+router.post("/", validatorCreateItem, createItem);
 
 module.exports = router;
