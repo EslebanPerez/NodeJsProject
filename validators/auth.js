6b0438249c517569f5082bb23cc3c2891cb23d39
EslebanPerez <eslebanperez@gmail.com>
2022-11-10 23:55:48 -0600

    Agregando validador para registro de usuarios

---
+++ validators/auth.js
@@ -0,0 +1,22 @@
+const {check} = require ('express-validator');
+const validateResults = require ("../utils/handleValidator");
+
+const validatorRegisterUser = [
+    check("name").exists().notEmpty().isLength({min:3, max:50}),
+    check("age").exists().notEmpty().isNumeric({min:3, max:50}),
+    check("password").exists().notEmpty().isLength({min:3, max:15}),
+    check("email").exists().notEmpty().isEmail(),
+    (req, res, next)=>{
+        return validateResults(req, res, next);
+    }
+]
+
+const validatorLogin = [
+    check("email").exists().notEmpty().isEmail(),
+    check("password").exists().notEmpty().isLength({min:3, max:15}),
+    (req, res, next)=>{
+        return validateResults(req, res, next);
+    }
+]
+
+module.exports = {validatorRegisterUser, validatorLogin}
