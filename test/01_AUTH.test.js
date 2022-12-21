8bee8969be81ea07b43070fc945a004f9dd14e6a
EslebanPerez <eslebanperez@gmail.com>
2022-12-20 19:22:30 -0600

    Agregando prueba en Login para validar una contraseña invalilda

--- test/01_AUTH.test.js
+++ test/01_AUTH.test.js
@@ -3,7 +3,7 @@ const app = require("../app");
 const {userModel} = require("../models")
 
 const testAuthLogin = {
-  "email": "test2@test.com",
+  "email": "test@test.com",
   "password": "123456789"
 }
 
@@ -20,26 +20,35 @@ const testAuthRegister = {
  * la DB
  */
 beforeAll(async() => {
-  await userModel.deleteMany()
+  await userModel.deleteMany({})
 })
 
 describe("[AUTH] esta es la prueba de /api/auth", () =>{
 
   test("Fallo para iniciar sesión para usuario inexistente", async() => {
     const response = await request(app)
-    .post("/api/auth/login")
-    .send(testAuthLogin)
+      .post("/api/auth/login")
+      .send(testAuthLogin)
     expect(response.statusCode).toEqual(404)
   });
 
   test("Registro de un usuario satisfactoriamente", async() => {
     const response = await request(app)
-    .post("/api/auth/register")
-    .send(testAuthRegister)
+      .post("/api/auth/register")
+      .send(testAuthRegister)
     expect(response.statusCode).toEqual(201);
     expect(response.body).toHaveProperty("data")
     expect(response.body).toHaveProperty("data.token")
     expect(response.body).toHaveProperty("data.user")
+  });
+
+  test("Login con password no valido", async () => {
+    const newTestAuthLogin = {...testAuthLogin, "password":"12345678"}
+    const response = await request(app)
+      .post("/api/auth/login")
+      .send(newTestAuthLogin);
+    expect(response.statusCode).toEqual(401);
+    expect(response.text).toContain('PASSWORD_INVALID');
   });
 
 })
--- test/app.test.js
+++
@@ -1,9 +0,0 @@
-describe("Prueba general", () =>{
-	test("Primera prueba", () =>{
-		const a = 4;
-		const b = 4;
-		const total = a+b;
-		expect(total).toEqual(8)
-	})
-})
-
