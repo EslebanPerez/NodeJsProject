bac7eebf707838d9823fca90aaa7b19202d6f3d1
EslebanPerez <eslebanperez@gmail.com>
2022-12-20 22:55:29 -0600

    Modificando express validator en registrar usuario

--- validators/auth.js
+++ validators/auth.js
@@ -4,6 +4,7 @@ const validateResults = require ("../utils/handleValidator");
 const validatorRegisterUser = [
     check("name").exists().notEmpty().isLength({min:3, max:50}),
     check("age").exists().notEmpty().isNumeric({min:3, max:50}),
+    check("role").optional(),
     check("password").exists().notEmpty().isLength({min:3, max:15}),
     check("email").exists().notEmpty().isEmail(),
     (req, res, next)=>{
